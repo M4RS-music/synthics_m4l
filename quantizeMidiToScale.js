7f8cd326c9523026a9afcd50a62c690c9db1a5ab
mars-janda <65905992+mars-janda@users.noreply.github.com>
2021-05-14 11:00:12 +0200

    Round frequencies from balls to midi notes
    Began quantize midi notes to scale js object that will let the user select the scale the bouncing balls will generate

---
+++ quantizeMidiToScale.js
@@ -0,0 +1,12 @@
+var major[], currentScale[];
+major = [2,2,1,2,2,2,1];
+
+function setRoot(r){
+  calculateScale(r);
+}
+
+function calculateScale(r){
+  for(var i=0; i<major.length(); i++){
+    
+  }
+}
